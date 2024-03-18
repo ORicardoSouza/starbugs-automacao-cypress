316e67c330ebd3c5efc12491622250d85afd8a7f
ricardo souza <ricardo.souza@senac.br>
2024-03-18 14:01:58 -0300

    inclusão de elementos allure para relatórios

--- cypress/e2e/camposObrigatorios/Test-requeireFields.spec.cy.js
+++ cypress/e2e/camposObrigatorios/Test-requeireFields.spec.cy.js
@@ -1,15 +1,19 @@
 import * as allure from "allure-cypress";
 
-describe('Campos obrigatorios', () => {
+describe('Validação de itens obrigatorios', () => {
   beforeEach(() => {
-    cy.section(`Acessando tela principal`)
+    cy.section(`Acessando tela principal`);
     cy.visit('/');
   });
-  context('Pagamento Via Cartão de Crédito', () => {
-
-    it('Comprar Expresso Tradicional', () => {
-      cy.selectProduct('Expresso Tradicional')
-      cy.deliveryAddressRequire()
+  context('Campos obrigatorios [ CEP , Número da casa , forma de pagamento ]', () => {
+    it('Validação do campos obrigatorio', () => {
+      cy.selectProduct('Expresso Tradicional');
+      cy.deliveryAddressRequire();
     });
+  });
+  it('Validação do campo obrigatorio do CEP', () => {
+    cy.selectProduct('Expresso Tradicional');
+    cy.get('input[value="Buscar CEP"]').click({ force: true });
+    cy.contains('p', 'Informe um CEP válido').should('exist')
   });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,76 +1,73 @@
 /// <reference types="cypress"/>
 import * as allure from "allure-cypress";
 
-
-Cypress.Commands.add('selectProduct', (nomeProduct) => {
+Cypress.Commands.add(`selectProduct`, (nomeProduct) => {
+  allure.feature(`Tipos De Produtos`)
   cy.step(`Selecionando produto ${nomeProduct}`)
   cy.contains(`${nomeProduct}`)
-    .parents('.coffee-item')
-    .find('.buy-coffee')
+    .parents(`.coffee-item`)
+    .find(`.buy-coffee`)
     .click({ force: true });
 });
 
-Cypress.Commands.add('deliveryAddress', () => {
-  allure.feature('Dados de Enedereço')
-  cy.step(`Inserindo dados de Endereço para entrega`)
-  cy.url().should('include','app/checkout')
+Cypress.Commands.add(`deliveryAddress`, () => {
+  allure.feature(`Dados de Enedereço`)
+  cy.step(`Inserindo dados de Endereço para entrega`);
+  cy.url().should(`include`,`app/checkout`);
   const street = `Rua Ginaldo Willis Galdino`
   const district = `Jardim Antártica`
   const city = `São Paulo`
   const uf = `SP`
 
-  cy.get('input[name="number"]').type(`12`);
-
-
-  cy.get('input[name="cep"]').type('02652190');
-  cy.get('input[value="Buscar CEP"]').click({ force: true });
-  cy.get('input[name="street"]').should('have.value', `${street}`);
-  cy.get('input[name="district"]').should('have.value', `${district}`);
-  cy.get('input[name="city"]').should('have.value', `${city}`);
-  cy.get('input[name="uf"]').should('have.value', `${uf}`)
-
+  cy.get(`input[name="number"]`).type(`12`);
+  cy.get(`input[name="cep"]`).type(`02652190`);
+  cy.get(`input[value="Buscar CEP"]`).click({ force: true });
+  cy.get(`input[name="street"]`).should(`have.value`, `${street}`);
+  cy.get(`input[name="district"]`).should(`have.value`, `${district}`);
+  cy.get(`input[name="city"]`).should(`have.value`, `${city}`);
+  cy.get(`input[name="uf"]`).should(`have.value`, `${uf}`);
 });
 
-Cypress.Commands.add('selectPaymentType', (type) => {
-  allure.feature('Tipos De Pagamento')
-  cy.step(`Selecionado Tipo de pagamento ${type}`)
+Cypress.Commands.add(`selectPaymentType`, (type) => {
+  allure.feature(`Uso do tipo de Pagamento ${type}`)
+  cy.step(`Selecionado Tipo de pagamento - ${type}`)
   switch (type) {
-    case 'Cartão de Crédito':
-      cy.get('label[for=credit]').click();
+    case `Cartão de Crédito`:
+      cy.get(`label[for=credit]`).click();
       break;
-    case 'Cartão de Débito':
-      cy.get('label[for=debit]').click();
+    case `Cartão de Débito`:
+      cy.get(`label[for=debit]`).click();
       break;
-    case 'À vista no PIX':
-      cy.get('label[for=pix]').click();
+    case `À vista no PIX`:
+      cy.get(`label[for=pix]`).click();
       break;
     default:
       throw new Error(`Unsupported payment type: ${paymentType}`);
   }
-})
+});
 
-Cypress.Commands.add('placeOrder', () => {
+Cypress.Commands.add(`placeOrder`, () => {
   //https://starbugs-qa.vercel.app/order-success
-  cy.step(`Confirmando Pedido`)
-  cy.get('.sc-idXgbr').click()
-  cy.url().should('include',`/order-success`)
+  cy.step(`Confirmando Pedido`);
+  cy.get(`.sc-idXgbr`).click();
+  cy.url().should(`include`,`/order-success`);
+});
+
+Cypress.Commands.add(`confirmOrder`, (selectPaymentType) => {
+  cy.placeOrder();
+  cy.step(`Confirmando dados do Pedido`);
+  cy.contains(`h1`, `Uhull! Pedido confirmado`).should(`be.visible`);
+  cy.get(`.sc-lllmON .sc-ipEyDJ`).should(`contain`, `Previsão de entrega`)
+    .and(`contain`, `20 min - 30 min`)
+    .and(`contain`, `Pagamento na entrega`)
+    .and(`contain`, `${selectPaymentType}`);
 });
 
 
-Cypress.Commands.add('confirmOrder', (selectPaymentType) => {
-  cy.placeOrder()
-  cy.step(`Confirmando dados do Pedido`)
-  cy.contains(`h1`, `Uhull! Pedido confirmado`).should('be.visible')
-  cy.get('.sc-lllmON .sc-ipEyDJ').should('contain', 'Previsão de entrega')
-    .and('contain', '20 min - 30 min')
-    .and('contain', 'Pagamento na entrega')
-    .and('contain', `${selectPaymentType}`)
-});
-
-
-Cypress.Commands.add('deliveryAddressRequire', () => {
-  cy.get('.sc-idXgbr').click()
-  cy.contains('p', 'Informe um CEP válido').should('exist')
-  cy.contains('p', 'Informe o número').should('exist')
-  cy.contains('p', 'Informe o método de pagamento').should('exist')
+Cypress.Commands.add(`deliveryAddressRequire`, () => {
+  allure.feature(`Campos Obrigatórios`);
+  cy.get(`.sc-idXgbr`).click();
+  cy.contains(`p`, `Informe um CEP válido`).should(`exist`);
+  cy.contains(`p`, `Informe o número`).should(`exist`);
+  cy.contains(`p`, `Informe o método de pagamento`).should(`exist`);
 });
